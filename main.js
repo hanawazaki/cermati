dd8314f2c1c239b676f511be23c620a82061b650
Hairul Ardhan Nawazaki <hanawazaki002@gmail.com>
2021-12-03 23:46:14 +0700

    newsletter panel + fix notif responsive

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-let notif = document.querySelector('.notif');
-let heroOverlay = document.querySelector('.overlay');
+const notif = document.querySelector('.notif');
+const heroOverlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('#close_btn')
+const panel = document.querySelector(".newsletter")
+const hero = document.querySelector(".hero")
+const highlights = document.querySelector(".highlights")
+
+function myFunction() {
+  const body = document.querySelector("body")
+
+  let x = body.scrollTop;
+  // if (scroll =)
+  console.log(x)
+  panel.classList.toggle("showPanel")
+}
+
+// hero.addEventListener("scroll", function (e) {
+//   e.preventDefault();
+//   console.log("scrolled")
+//   panel.classList.toggle("showPanel")
+
+// })
+
+closeBtn.addEventListener("click", function (event) {
+  event.preventDefault()
+  panel.classList.toggle("closePanel")
+});
 
 const closeNotif = () => {
   notif.classList.toggle("hide")
